44d91cff84a94348070e6fecc0ca8acf940c07a4
Donald Mennerich <donaldmennerich@nypl.org>
2012-10-12 09:27:53 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arch::Application.config.secret_token = 'f249ff4fdbb4a65e4a64f2ccabf08d6cfbcc745b3c7c3730cf2480378e54d7c198f17bfd1080d8415162e132543d1fb387b2fb9636e3e5dd519a4371a9e1c725'
