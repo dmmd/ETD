d1d5423d6d50168b9801775a5ca707a4364571ac
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2012-10-12 11:24:30 -0400

    adds ETD show view

--- app/controllers/etds_controller.rb
+++ app/controllers/etds_controller.rb
@@ -7,10 +7,14 @@ class EtdsController < ApplicationController
   def create
     @etd = Etd.create(params[:etd])
     flash[:notice] = 'ETD created'
-    redirect_to root_path 
+    redirect_to root_path
   end
 
   def index
     @etds = Etd.find(:all)
+  end
+
+  def show
+    @etd = Etd.find(params[:id])
   end
 end
